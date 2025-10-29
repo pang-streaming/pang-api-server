cf80cb290ebc855c578caaf94f6b6b8a732bc77c
kmg <legolove08@naver.com>
2025-10-29 11:11:18 +0900

    init

---
+++ pang-api/src/test/java/com/pang/pangapi/PangApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pang.pangapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ pang-gateway/src/main/java/com/pang/panggateway/PangGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pang.panggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PangGatewayApplication.class, args);
+    }
+
+}
