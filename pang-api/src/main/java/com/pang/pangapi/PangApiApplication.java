8e66341afee38117bbb19d959d9d5d9da8389d05
kmg <legolove08@naver.com>
2025-11-04 11:23:16 +0900

    fix: gradle

---
+++ pang-api/src/main/java/com/pang/pangapi/PangApiApplication.java
@@ -0,0 +1,13 @@
+package com.pang.pangapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PangApiApplication.class, args);
+    }
+
+}
